98d22e4b9a90aab635da72fc9a79ed04fe5414d2
khilmanoj <33872950+khilmanoj@users.noreply.github.com>
2021-06-09 16:41:19 -0400

    Update main.tf

--- openstacksinglevm/main.tf
+++ openstacksinglevm/main.tf
@@ -47,13 +47,13 @@ provider "openstack" {
 
 resource "openstack_compute_instance_v2" "single-vm" {	
   #name      = "${ length(var.instance_name) > 0 ? var.instance_name : format("terraform-single-vm-${random_id.random_padding.hex}-%02d", count.index+1)}"
-  name      = "${var.instance_name}"
-  image_id  = "${var.openstack_image_id}"
-  flavor_id = "${var.openstack_flavor_id}"
-  key_pair  = "${var.key_pair_name}"
+  name      = "var.instance_name"
+  image_id  = "var.openstack_image_id"
+  flavor_id = "var.openstack_flavor_id"
+  key_pair  = "var.key_pair_name"
 
   network {
-    name = "${var.openstack_network_name}"
+    name = "var.openstack_network_name"
   }
 }
 
